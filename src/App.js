7a70e014244604a6de59718f131c733924b473e1
MADPC <amindahham@gmail.com>
2024-02-03 12:12:28 +0100

    added Features to App

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import "./assets/js/script";
 import Services from "./components/Services/Services";
 import About from "./components/About/About";
 import Qualities from "./components/Qualities/Qualities";
+import Features from "./components/Features/Features";
+
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-        <Services/>
-        <About/>
-      <Qualities/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Services/>
+            <About/>
+            <Qualities/>
+            <Features/>
+        </div>
+    );
 }
 
 export default App;
