9409c96869364b35ca9f55ade9c063cd44085992
MADPC <amindahham@gmail.com>
2024-02-03 12:11:58 +0100

    tried to solve Buttons problem

--- src/components/Headers/Header.jsx
+++ src/components/Headers/Header.jsx
@@ -3,6 +3,10 @@ import Navbar from "../Navbar/Navbar";
 import "./Header.css"
 import {FaPaperPlane} from "react-icons/fa";
 
+const handleButtonClick = (event) => {
+    event.preventDefault();
+    // Add your button click logic here
+};
 const Header = () => {
     return (
         <header className='header flex flex-center flex-column'>
@@ -12,9 +16,9 @@ const Header = () => {
                     <h1 className='text-uppercase header-title'>Dahham Company</h1>
                     <p className='text-lead'>We are the oldest company in Syria specialized in manufacturing washing
                         machine parts.</p>
-                    <p></p>
-                    <a href="/" className='btn header-btn btn-blue'>
-                        <FaPaperPlane/> <span>get started</span>
+
+                    <a href="/" className='btn header-btn button btn-blue' onClick={handleButtonClick}>
+                        <FaPaperPlane/> <span>get started </span>
                     </a>
                 </div>
             </div>
